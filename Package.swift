13b0cd6d2044bdd04f72aa3b308e8febac352f90
Kojirou <kojiroujyy@vip.qq.com>
2016-07-15 13:54:46 +0800

    add libmongoc

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "SFMongo"
+    name: "SFMongo",
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-mongo-c.git", versions: Version(0,0,0)..<Version(10,0,0))
+    ]
 )
