074d3328849dfa2204186cf8500e5e3a08d77c8c
Kojirou <kojiroujyy@vip.qq.com>
2016-07-19 09:14:54 +0800

    update

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "SFMongo",
+    targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-mongo-c.git", versions: Version(0,0,0)..<Version(10,0,0))
     ]
