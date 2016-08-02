60d04219c2634f87877f619b91cada15bf006496
Kojirou <kojiroujyy@vip.qq.com>
2016-08-02 15:18:39 +0800

    add URLResponseRepresentable

---
+++ Sources/URLResponseRepresentable.swift
@@ -0,0 +1,20 @@
+//
+//  URLResponseRepresentable.swift
+//  SFPushService
+//
+//  Created by Kojirou on 16/7/28.
+//
+//
+
+import Foundation
+
+protocol URLResponseRepresentable {
+    var responseBody: String {get}
+    var responseDictionary: Dictionary<String, JSONStringConvertible?> {get}
+}
+
+extension URLResponseRepresentable {
+    var responseBody: String {
+        return responseDictionary.jsonString
+    }
+}
