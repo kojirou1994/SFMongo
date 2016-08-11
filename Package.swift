0f7f57286f0007366d74701de29a235b84460672
Kojirou <kojiroujyy@vip.qq.com>
2016-08-11 10:31:53 +0800

    add linux mongo-c

--- Package.swift
+++ Package.swift
@@ -1,10 +1,22 @@
 import PackageDescription
 
+#if os(OSX)
 let package = Package(
     name: "SFMongo",
     targets: [],
-    dependencies: [
+    dependencies: [  
         .Package(url: "https://github.com/PerfectlySoft/Perfect-mongo-c.git", versions: Version(0,0,0)..<Version(10,0,0)),
         .Package(url: "https://github.com/kojirou1994/SFJSON.git", versions: Version(0,0,0)..<Version(10,0,0))
     ]
 )
+#else
+let package = Package(
+    name: "SFMongo",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-mongo-c-linux.git", versions: Version(0,0,0)..<Version(10,0,0)),
+        .Package(url: "https://github.com/kojirou1994/SFJSON.git", versions: Version(0,0,0)..<Version(10,0,0))
+    ]
+)
+#endif
+
